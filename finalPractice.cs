9072e03284427df976c246e94abe5addd38e7f4e
Saarnaaja <Norilsk.Devil@gmail.com>
2021-12-28 11:45:14 +0700

    Выполнена практика модуля (файл finalPractice.cs)

---
+++ finalPractice.cs
@@ -0,0 +1,20 @@
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+
+        Console.WriteLine($"Your name is {name} and age is {age}");
+
+        Console.Write("Enter your birthdate: ");
+        var birthdate = Console.ReadLine();
+
+        Console.WriteLine($"Your birthdate is {birthdate}");
+    }
+}
