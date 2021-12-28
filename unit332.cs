4f94f2c8bf291efd683b21447bd489dc30c8c2b3
Saarnaaja <Norilsk.Devil@gmail.com>
2021-12-28 10:08:57 +0700

    Выполнено упражнение 3.3.2

---
+++ unit332.cs
@@ -0,0 +1,17 @@
+using System;
+
+class MainClass 
+{
+	public static void Main (string[] args) 
+	{
+		string MyName = "Andrey";
+		byte MyAge = 25;
+		bool HaveIApet  = false;
+		double MyShoeSize = 45;
+
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
+	}
+}
